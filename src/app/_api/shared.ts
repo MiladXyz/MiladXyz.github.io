053bfa5ef3aae3b4ffd677c4972ea70e8f57827f
Milad <milad.bhzz@gmail.com>
2024-12-27 16:59:09 +0330

    change env

--- src/app/_api/shared.ts
+++ src/app/_api/shared.ts
@@ -1,3 +1,3 @@
 export const GRAPHQL_API_URL = process.env.NEXT_BUILD
-  ? `https://hoffen.vercel.app`
-  : process.env.NEXT_PUBLIC_SERVER_URL
+  ? process.env.PAYLOAD_PUBLIC_SERVER_URL
+  : `https://hoffen.vercel.app`
