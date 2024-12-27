381738b724772e44a2b7322352516620ec6cae8b
Milad <milad.bhzz@gmail.com>
2024-12-27 16:06:58 +0330

    change api url

--- src/app/_api/shared.ts
+++ src/app/_api/shared.ts
@@ -1,1 +1,3 @@
-export const GRAPHQL_API_URL = process.env.NEXT_PUBLIC_SERVER_URL
+export const GRAPHQL_API_URL = process.env.NEXT_BUILD
+  ? `http://localhost:${process.env.PORT || 3000}`
+  : process.env.NEXT_PUBLIC_SERVER_URL
